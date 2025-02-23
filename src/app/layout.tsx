ed7898387050746c6a473daa21d11487a242fb9b
ayushpathak-48 <ayush010pathak@gmail.com>
2025-02-23 17:36:19 +0530

    fix width issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       >
         <NuqsAdapter>
           <ClientProvider>
-            <div className="md:w-[calc(100%-16rem)]">
+            <div className="md:w-[calc(100%-16rem)] w-full">
               <Navbar />
               <main className="w-full">{children}</main>
             </div>
