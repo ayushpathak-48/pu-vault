669b4526b052a81328ed913bb2814e324459edcf
kachakaran6 <kachakaran06@gmail.com>
2025-02-23 15:06:59 +0530

    Division A,B,C Completed

--- src/lib/constants/time-table.ts
+++ src/lib/constants/time-table.ts
@@ -39,6 +39,7 @@ export const time_table = [
           label: "Saturday",
         },
       ],
+      // 7:30 : 8:30
       [
         {
           id: 1,
@@ -84,12 +85,10 @@ export const time_table = [
         },
         {
           id: 8,
-          label: "DCN",
-          professor: "Sonali Gupta",
-          classroom: "305",
-          cellClassName: "text-purple-500",
+          label: "PULMS Self Learning",
         },
       ],
+      //  8:30 : 9:30
       [
         {
           id: 1,
@@ -140,6 +139,7 @@ export const time_table = [
           cellClassName: "text-sm",
         },
       ],
+      // 9:30 TO 9:45
       [
         {
           id: 1,
@@ -151,6 +151,7 @@ export const time_table = [
           col_span: 7,
         },
       ],
+      //  9.45 TO 10.45
       [
         {
           id: 1,
@@ -202,6 +203,7 @@ export const time_table = [
           cellClassName: "text-orange-500",
         },
       ],
+      // 10:45 - 11:45
       [
         {
           id: 1,
@@ -237,6 +239,7 @@ export const time_table = [
           cellClassName: "text-purple-500",
         },
       ],
+      // 11:45 - 12:45
       [
         {
           id: 1,
@@ -248,6 +251,7 @@ export const time_table = [
           col_span: 7,
         },
       ],
+      // 12:45 - 1:35
       [
         {
           id: 1,
@@ -300,6 +304,7 @@ export const time_table = [
           row_span: 2,
         },
       ],
+      //  1:35 - 2:25
       [
         {
           id: 1,
@@ -361,6 +366,7 @@ export const time_table = [
           label: "Saturday",
         },
       ],
+      //  7:30 - 8:30
       [
         {
           id: 1,
@@ -373,44 +379,45 @@ export const time_table = [
         {
           id: 3,
           label: "CG",
-          professor: "Prof. Preeti",
+          professor: "Preeti",
           classroom: "307",
-          cellClassName: "text-purple-500",
+          cellClassName: "text-orange-500",
         },
         {
           id: 4,
           label: "CG LAB",
-          professor: "Prof. Preeti, Prof. Ghanshyam",
+          professor: [" Preeti, Ghanshyam"],
           lab: ["603", "604"],
-          cellClassName: "text-[#11131e]",
+          cellClassName: "text-orange-500",
+          row_span: 2,
         },
         {
           id: 5,
           label: "DCN",
-          professor: "Prof. Komal B",
+          professor: "Komal B",
           classroom: "307",
           cellClassName: "text-purple-500",
         },
         {
           id: 6,
           label: "CG",
-          professor: "Prof. Preeti",
+          professor: "Preeti",
           classroom: "307",
-          cellClassName: "text-purple-500",
+          cellClassName: "text-orange-500",
         },
         {
           id: 7,
           label: "DCN",
-          professor: "Prof. Komal B",
+          professor: "Komal B",
           classroom: "306",
           cellClassName: "text-purple-500",
         },
         {
           id: 8,
           label: "PULMS Self Learning",
-          cellClassName: "text-[#11131e]",
         },
       ],
+      //  8:30 - 9:30
       [
         {
           id: 1,
@@ -423,35 +430,28 @@ export const time_table = [
         {
           id: 3,
           label: "Specialization AI",
-          professor: "Prof. Rinkal S.",
+          professor: "Rinkal S.",
           classroom: "307",
           cellClassName: "text-[#11131e]",
         },
         {
           id: 4,
-          label: "CG LAB",
-          professor: "Prof. Preeti, Dr. Ghanshyam",
-          lab: ["603", "604"],
+          label: "Specialization AI",
+          professor: "Rinkal S.",
+          classroom: "307",
           cellClassName: "text-[#11131e]",
         },
         {
           id: 5,
           label: "Specialization AI",
-          professor: "Prof. Rinkal S.",
+          professor: "Rinkal S.",
           classroom: "307",
           cellClassName: "text-[#11131e]",
         },
         {
           id: 6,
           label: "Specialization AI",
-          professor: "Prof. Rinkal S.",
-          classroom: "307",
-          cellClassName: "text-[#11131e]",
-        },
-        {
-          id: 7,
-          label: "Specialization AI",
-          professor: "Prof. Rinkal S.",
+          professor: "Rinkal S.",
           classroom: "306",
           cellClassName: "text-[#11131e]",
         },
@@ -461,6 +461,7 @@ export const time_table = [
           cellClassName: "text-sm",
         },
       ],
+      //  9:30 - 9:45
       [
         {
           id: 1,
@@ -472,6 +473,7 @@ export const time_table = [
           col_span: 7,
         },
       ],
+      //  9:45 - 10:45
       [
         {
           id: 1,
@@ -484,20 +486,21 @@ export const time_table = [
         {
           id: 3,
           label: "CA",
-          professor: "Prof. Smarika Rai",
+          professor: "Smarika Rai",
           cellClassName: "text-[#11131e]",
         },
         {
           id: 4,
           label: "DCN LAB",
-          professor: "Prof. Komal B, Prof. Sheeja",
+          professor: ["Komal B, Sheeja"],
           lab: ["712", "713"],
           cellClassName: "text-purple-500",
+          row_span: 2,
         },
         {
           id: 5,
           label: "CA",
-          professor: "Prof. Smarika Rai",
+          professor: "Smarika Rai",
           cellClassName: "text-[#11131e]",
         },
         {
@@ -508,17 +511,20 @@ export const time_table = [
         {
           id: 7,
           label: "AJP LAB",
-          professor: "Prof. Rinku, Prof. Arun",
+          professor: ["Rinku, Arun"],
           lab: ["702", "703"],
-          cellClassName: "text-purple-500",
+          cellClassName: "text-red-500",
+          row_span: 2,
         },
         {
           id: 8,
           label: "DCN",
-          professor: "Prof. Komal B.",
-          cellClassName: "text-[#11131e]",
+          professor: "Komal B.",
+          classroom: "307",
+          cellClassName: "text-purple-500",
         },
       ],
+      // 10:45 - 11:45
       [
         {
           id: 1,
@@ -531,42 +537,27 @@ export const time_table = [
         {
           id: 3,
           label: "PULMS Self Learning",
-          cellClassName: "text-[#11131e]",
         },
         {
           id: 4,
-          label: "DCN LAB",
-          professor: "Prof. Komal B, Prof. Sheeja",
-          lab: ["712", "713"],
-          cellClassName: "text-purple-500",
+          label: "PULMS Self Learning",
         },
         {
           id: 5,
-          label: "PULMS Self Learning",
-          cellClassName: "text-red-500",
+          label: "CDC English & Mathematical Aptitude",
+          professor: "Arul P",
+          classroom: "603",
+          cellClassName: "text-[#11131e]",
         },
         {
           id: 6,
-          label: "CDC English & Mathematical Aptitude",
-          professor: "Prof. Arul P",
-          classroom: "603",
-          cellClassName: "text-purple-500",
-        },
-        {
-          id: 7,
-          label: "AJP LAB",
-          professor: "Prof. Rinku, Prof. Arun",
-          lab: ["702", "703"],
-          cellClassName: "text-purple-500",
-        },
-        {
-          id: 8,
           label: "CG",
-          professor: "Prof. Preeti",
+          professor: "Preeti",
           classroom: "307",
-          cellClassName: "text-purple-500",
+          cellClassName: "text-orange-500",
         },
       ],
+      // 11:45 - 12:45
       [
         {
           id: 1,
@@ -578,6 +569,7 @@ export const time_table = [
           col_span: 7,
         },
       ],
+      // 12:45 -  1:35
       [
         {
           id: 1,
@@ -590,44 +582,47 @@ export const time_table = [
         {
           id: 3,
           label: "AJP Lab",
-          professor: "Prof. Rinku P., Prof. Arun U.",
+          professor: ["Rinku P., Arun U."],
           lab: ["610", "611"],
           cellClassName: "text-red-500",
+          row_span: 2,
         },
         {
           id: 4,
           label: "Specialization AI Lab",
-          professor: "Prof. Rinkal S., Prof. Niharika",
+          professor: ["Rinkal S., Niharika"],
           lab: ["708", "709"],
           cellClassName: "text-[#11131e]",
+          row_span: 2,
         },
         {
           id: 5,
           label: "Value Added",
-          cellClassName: "txt-sm",
+          row_span: 2,
         },
         {
           id: 6,
           label: "AJP",
-          professor: "Prof. Rinku P.",
+          professor: "Rinku P.",
           classroom: "307",
           cellClassName: "text-red-500",
         },
         {
           id: 7,
           label: "AJP",
-          professor: "Prof. Rinku P.",
-          classroom: "307",
+          professor: "Rinku P.",
+          classroom: "306",
           cellClassName: "text-red-500",
         },
         {
           id: 8,
           label: "AJP",
-          professor: "Prof. Rinku P.",
+          professor: "Rinku P.",
           classroom: "307",
           cellClassName: "text-red-500",
         },
       ],
+      // 1:35 - 2:25
       [
         {
           id: 1,
@@ -639,44 +634,353 @@ export const time_table = [
         },
         {
           id: 3,
-          label: "AJP Lab",
-          professor: "Prof. Rinku P., Prof. Arun U.",
-          lab: ["610", "611"],
-          cellClassName: "text-red-500",
-        },
-        {
-          id: 4,
-          label: "Specialization AI Lab",
-          professor: "Prof. Rinkal S., Prof. Niharika",
-          lab: ["708", "709"],
-          cellClassName: "text-[#11131e]",
-        },
-        {
-          id: 5,
-          label: "Value Added",
-          cellClassName: "text-sm",
-        },
-        {
-          id: 6,
           label: "Face",
           professor: "Harshad",
           classroom: "304",
-          cellClassName: "text-sm",
+          cellClassName: "text-[#11131e]",
         },
         {
-          id: 7,
+          id: 4,
           label: "Library",
           cellClassName: "text-sm",
         },
         {
           id: 8,
           label: "CA",
-          professor: "Prof. Smarika Rai",
+          professor: "Smarika Rai",
           classroom: "307",
-          cellClassName: "text-sm",
+          cellClassName: "text-[#11131e]",
         },
       ],
     ],
   },
-  //
+  // Division - C
+  {
+    id: 3,
+    title: "Division C",
+    division_key: "div_c",
+    data: [
+      [
+        {
+          id: 1,
+          label: "Lecture No.",
+        },
+        {
+          id: 2,
+          label: "Lecture Timing",
+        },
+        {
+          id: 3,
+          label: "Monday",
+        },
+        {
+          id: 4,
+          label: "Tuesday",
+        },
+        {
+          id: 5,
+          label: "Wednesday",
+        },
+        {
+          id: 6,
+          label: "Thursday",
+        },
+        {
+          id: 7,
+          label: "Friday",
+        },
+        {
+          id: 8,
+          label: "Saturday",
+        },
+      ],
+      //  7:30 - 8:30
+      [
+        {
+          id: 1,
+          label: "1",
+        },
+        {
+          id: 2,
+          label: "07.30 TO 08.30",
+        },
+        {
+          id: 3,
+          label: "AJP",
+          professor: "Honey",
+          classroom: "308",
+          cellClassName: "text-red-500",
+        },
+        {
+          id: 4,
+          label: "DCN",
+          professor: "Sheeja C",
+          classroom: "308",
+          cellClassName: "text-purple-500",
+        },
+        {
+          id: 5,
+          label: "Specialization AI",
+          professor: "Nirmit Shah",
+          classroom: "308",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 6,
+          label: "Specialization AI",
+          professor: "Nirmit Shah",
+          classroom: "308",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 7,
+          label: "AJP",
+          professor: "Honey",
+          classroom: "308",
+          cellClassName: "text-red-500",
+        },
+        {
+          id: 8,
+          label: "Self Learning",
+        },
+      ],
+      // 8:30 - 9:30
+      [
+        {
+          id: 1,
+          label: "2",
+        },
+        {
+          id: 2,
+          label: "08.30 TO 09.30",
+        },
+        {
+          id: 3,
+          label: "CA",
+          professor: "Smarika Rai",
+          classroom: "308",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 4,
+          label: "AJP",
+          professor: "Honey",
+          classroom: "308",
+          cellClassName: "text-red-500",
+        },
+        {
+          id: 5,
+          label: "CG",
+          professor: "Dampy",
+          classroom: "308",
+          cellClassName: "text-orange-500",
+        },
+        {
+          id: 6,
+          label: "CG",
+          professor: "Dampy",
+          classroom: "308",
+          cellClassName: "text-orange-500",
+        },
+        {
+          id: 7,
+          label: "DCN",
+          professor: "Sheeja C",
+          classroom: "308",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 8,
+          label: "Library",
+          cellClassName: "text-sm",
+        },
+      ],
+      //  9:30 - 9:45
+      [
+        {
+          id: 1,
+          label: "Recess",
+        },
+        {
+          id: 2,
+          label: "Short Break (09:30 TO 09:45)",
+          col_span: 7,
+        },
+      ],
+      // 9:45 - 10:45
+      [
+        {
+          id: 1,
+          label: "3",
+        },
+        {
+          id: 2,
+          label: "09.45 TO 10.45",
+        },
+        {
+          id: 3,
+          label: "Specialization AI Lab",
+          professor: ["Nirmit Shah", "Prachi M."],
+          lab: ["706", "707"],
+          cellClassName: "text-[#11131e]",
+          row_span: 2,
+        },
+        {
+          id: 4,
+          label: "Value Added Lab",
+          lab: ["708", "709"],
+          row_span: 2,
+        },
+        {
+          id: 5,
+          label: "AJP Lab",
+          professor: ["Honey P.", "Arun U."],
+          lab: ["706", "707"],
+          cellClassName: "text-red-500",
+          row_span: 2,
+        },
+        {
+          id: 6,
+          label: "Library",
+        },
+        {
+          id: 7,
+          label: "DCN",
+          professor: ["Sheeja C.", "Mayank P."],
+          lab: ["708", "709"],
+          cellClassName: "text-purple-500",
+          row_span: 2,
+        },
+        {
+          id: 8,
+          label: "DCN",
+          professor: "Sheeja C",
+          classroom: "308",
+          cellClassName: "text-purple-500",
+        },
+      ],
+      // 10:45 - 11:45
+      [
+        {
+          id: 1,
+          label: "4",
+        },
+        {
+          id: 2,
+          label: "10.45 TO 11.45",
+        },
+        {
+          id: 3,
+          label: "CG",
+          professor: "Dampy",
+          classroom: "306",
+          cellClassName: "text-orange-500",
+        },
+        {
+          id: 4,
+          label: "Specialization AI",
+          professor: "Nirmit Shah",
+          classroom: "308",
+          cellClassName: "text-[#11131e]",
+        },
+      ],
+      //  11:45 - 12:45
+      [
+        {
+          id: 1,
+          label: "Recess",
+        },
+        {
+          id: 2,
+          label: "Lunch Break (11:45 TO 12:45)",
+          col_span: 7,
+        },
+      ],
+      // 12:45 - 1:35
+      [
+        {
+          id: 1,
+          label: "5",
+        },
+        {
+          id: 2,
+          label: "12.45 TO 01.35",
+        },
+        {
+          id: 3,
+          label: "CA",
+          professor: "Smarika Rai",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 4,
+          label: "AJP Lab",
+          professor: ["Honey P.", "Arun U."],
+          lab: ["712", "713"],
+          cellClassName: "text-red-500",
+          row_span: 2,
+        },
+        {
+          id: 5,
+          label: "CG Lab",
+          professor: ["Dampy", "Preeti"],
+          lab: ["704", "705"],
+          cellClassName: "text-orange-500",
+        },
+        {
+          id: 6,
+          label: "PULMS Self Learning",
+        },
+        {
+          id: 7,
+          label: "FACE",
+          professor: "Harshad",
+          classroom: "603",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 8,
+          label: "CG",
+          professor: "Dampy",
+          classroom: "308",
+          cellClassName: "text-orange-500",
+        },
+      ],
+      // 1:35 - 2:25
+      [
+        {
+          id: 1,
+          label: "6",
+        },
+        {
+          id: 2,
+          label: "01.35 TO 02.25",
+        },
+        {
+          id: 3,
+          label: "CDC English & Mathematical Aptitude",
+          professor: "Arul P.",
+          classroom: "605",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 4,
+          label: "CA",
+          professor: "Samika Rai",
+          cellClassName: "text-[#11131e]",
+        },
+        {
+          id: 5,
+          label: "PULMS Self Learning",
+        },
+        {
+          id: 6,
+          label: "CA",
+          professor: "Samika Rai",
+          classroom: "308",
+          cellClassName: "text-[#11131e]",
+        },
+      ],
+    ],
+  },
 ];
