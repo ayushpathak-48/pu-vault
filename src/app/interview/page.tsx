d0e80f873ae9850c8e3ae8b0ba4ac761387fb6dc
kachakaran6 <kachakaran06@gmail.com>
2025-02-26 19:58:29 +0530

    interview section added

---
+++ src/app/interview/page.tsx
@@ -0,0 +1,36 @@
+import { interviewQuestions } from "@/lib/constants/interviewQuestions.constant";
+import Link from "next/link";
+import React from "react";
+
+const SoftwareLinksPage = () => {
+  return (
+    <div className="p-5 flex flex-col gap-10 w-full">
+      {/* <h2 className="text-2xl font-semibold custom-heading text-center">
+        All Assignments Link
+      </h2> */}
+      <div className="flex flex-col gap-4">
+        {interviewQuestions.map((software) => (
+          <div
+            key={software.id}
+            className="flex items-center justify-between bg-gray-100 p-4 rounded-lg"
+          >
+            <Link href={software.href}>
+              <h2 className="text-lg font-medium hover:text-sky-500 transition-all">
+                {software.title}
+              </h2>
+            </Link>
+            <Link
+              href={software.href}
+              target="_blank"
+              className="font-medium text-sky-600"
+            >
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SoftwareLinksPage;
