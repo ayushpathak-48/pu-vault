c2fa9c54dae4650c8e60e6c86ca40c86c49789d5
karan_kacha <85750282+kachakaran6@users.noreply.github.com>
2025-02-26 09:10:22 +0530

    Update assignments.constant.ts

--- src/lib/constants/assignments.constant.ts
+++ src/lib/constants/assignments.constant.ts
@@ -4,11 +4,11 @@ export const assignments = [
   {
     id: 1,
     title: "AJP assignment 1",
-    href: `${MATERIAL_LINK_BASE_URL}/assignments/ajp_assignment-1.pdf`,
+    href: `${MATERIAL_LINK_BASE_URL}/assignments/ajp-assignment-1.pdf`,
   },
   {
     id: 2,
     title: "Computer Graphics",
-    href: `${MATERIAL_LINK_BASE_URL}/assignments/ajp_assignment-1.pdf`,
+    href: `${MATERIAL_LINK_BASE_URL}/assignments/cg-assignment-1.pdf`,
   },
 ];
