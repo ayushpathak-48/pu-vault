4b5fd950eab4b0cbb92cbfcc94d3c4a2e4223d1f
ayushpathak-48 <ayush010pathak@gmail.com>
2025-03-04 01:07:19 +0530

    make source code navigator responsive sticky to top

--- src/app/code/[subjectId]/[practicalId]/page.tsx
+++ src/app/code/[subjectId]/[practicalId]/page.tsx
@@ -74,7 +74,7 @@ const CodePage = () => {
     <>
       <Tabs
         value={practical.key}
-        className="w-full px-2 flex items-center justify-center gap-3 sticky top-20 bg-gray-50 border-b z-[9] py-2"
+        className="w-full px-2 flex items-center justify-center gap-3 sticky top-16 md:top-20 bg-gray-50 border-b z-[9] py-2"
       >
         <Button
           disabled={currentPracticalIndex == 0}
