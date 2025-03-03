e472cc17f539c9bd55a851c101c0074942b1fdae
ayushpathak-48 <ayush010pathak@gmail.com>
2025-03-04 01:04:55 +0530

    make source code navigator sticky to top

--- src/app/code/[subjectId]/[practicalId]/page.tsx
+++ src/app/code/[subjectId]/[practicalId]/page.tsx
@@ -74,7 +74,7 @@ const CodePage = () => {
     <>
       <Tabs
         value={practical.key}
-        className="w-full px-2 flex items-center justify-center gap-3"
+        className="w-full px-2 flex items-center justify-center gap-3 sticky top-20 bg-gray-50 z-[9] py-2"
       >
         <Button
           disabled={currentPracticalIndex == 0}
