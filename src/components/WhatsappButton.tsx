a70f27ba18f14eedd350e97ee8c828305acd7620
kachakaran6 <kachakaran06@gmail.com>
2025-03-09 17:49:02 +0530

    add join whatsapp dialog

---
+++ src/components/WhatsappButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export const WhatsappButton = () => {
+  return (
+    <Button asChild variant={"outline"}>
+      <Link
+        target="_blank"
+        href={`https://chat.whatsapp.com/D88Nxz3agYV7or3fbUR9zc`}
+      >
+        <Image
+          src={"/icons/whatsapp.png"}
+          alt="Whatsapp"
+          height={20}
+          width={20}
+          className="size-5"
+        />{" "}
+        Join Now
+      </Link>
+    </Button>
+  );
+};
---
+++ src/components/whatsapp-dialog.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { WhatsappButton } from "./WhatsappButton";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export const WhatsappDialog = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const whatsappDialogClosed = localStorage.getItem("whatsappDialogClosed");
+    if (!whatsappDialogClosed) {
+      setOpen(true);
+    }
+  }, []);
+
+  const handleCloseClick = () => {
+    localStorage.setItem("whatsappDialogClosed", "true");
+    setOpen(false);
+  };
+
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogContent className="max-md:max-w-[75%]">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-center gap-1">
+            <Image
+              src={"/icons/whatsapp.png"}
+              alt="Whatsapp"
+              height={20}
+              width={20}
+              className="size-5"
+            />
+            Whatsapp Community
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Join whatsapp community for getting updates. Never miss any new
+            feature,updates,material.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={handleCloseClick}>
+            Already Joined
+          </AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <WhatsappButton />
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
