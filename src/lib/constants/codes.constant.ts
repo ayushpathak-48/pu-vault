855cd2d256da637fcce6aecda05d8b55789d7e35
Aayush Pathak <118837534+ayushpathak-48@users.noreply.github.com>
2025-03-19 21:21:55 +0530

    Update codes.constant.ts

--- src/lib/constants/codes.constant.ts
+++ src/lib/constants/codes.constant.ts
@@ -18,13 +18,13 @@ export const practicalCodes = [
             type: "code",
             fileName: "tic-tac-toe.py",
             value: `def print_board(board):
- print("\n")
+ print("\\n")
  print(f" {board[0]} | {board[1]} | {board[2]} ")
  print("---+---+---")
  print(f" {board[3]} | {board[4]} | {board[5]} ")
  print("---+---+---")
  print(f" {board[6]} | {board[7]} | {board[8]} ")
- print("\n")
+ print("\\n")
 
 def check_win(board, player):
     win_conditions = [
