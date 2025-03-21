40e05bf2bb29aec74f55742f8142ae9caee50455
Aayush Pathak <118837534+ayushpathak-48@users.noreply.github.com>
2025-03-21 17:28:16 +0530

    Update assignments.constant.ts

--- src/lib/constants/assignments.constant.ts
+++ src/lib/constants/assignments.constant.ts
@@ -2,32 +2,37 @@ import { MATERIAL_LINK_BASE_URL } from ".";
 
 export const assignments = [
   {
-    id: 1,
+    id: 7,
+    title: "Artificial Intelligence - Div C",
+    href: `${MATERIAL_LINK_BASE_URL}/assignments/ai-assignment-div-c.pdf`,
+  },
+  {
+    id: 6,
     title: "AJP assignment 1",
     href: `${MATERIAL_LINK_BASE_URL}/assignments/ajp-assignment-1.pdf`,
   },
   {
-    id: 2,
+    id: 5,
     title: "Computer Graphics",
     href: `${MATERIAL_LINK_BASE_URL}/assignments/cg-assignment-1.pdf`,
   },
   {
-    id: 3,
+    id: 4,
     title: "English and Mathematical Apptitude",
     href: `${MATERIAL_LINK_BASE_URL}/assignments/cdc-assignment.pdf`,
   },
   {
-    id: 4,
+    id: 3,
     title: "Artificial Intelligence",
     href: `${MATERIAL_LINK_BASE_URL}/assignments/ai-assignment.pdf`,
   },
   {
-    id: 5,
+    id: 2,
     title: "Cyber Security and Forensics",
     href: `${MATERIAL_LINK_BASE_URL}/assignments/csf-assignment.pdf`,
   },
   {
-    id: 6,
+    id: 1,
     title: "Software Engineering",
     href: `${MATERIAL_LINK_BASE_URL}/assignments/se-assignment.pdf`,
   },
