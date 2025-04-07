da669982ab86ae9d872a0139795a046464f1917d
ayushpathak-48 <ayush010pathak@gmail.com>
2025-04-07 21:50:33 +0530

    bug fix

--- src/app/flash-cards/[subjectId]/[unit]/page.tsx
+++ src/app/flash-cards/[subjectId]/[unit]/page.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 import CodePageClient from "./client";
+import { flashCards } from "@/lib/constants/flash-cards.constant";
+
+export async function generateStaticParams() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const paths: any = [];
+
+  flashCards.forEach((card) => {
+    card.units.forEach((unit) => {
+      paths.push({
+        subjectId: card.key,
+        unit: unit?.key,
+      });
+    });
+  });
+
+  return paths;
+}
 
 const page = async () => {
   return <CodePageClient />;
