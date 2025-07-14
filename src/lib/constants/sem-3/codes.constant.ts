887f72c1db291d62810a5012dd26865c20e7e09f
Aayush Pathak <118837534+ayushpathak-48@users.noreply.github.com>
2025-07-14 12:48:02 +0530

    Update codes.constant.ts

--- src/lib/constants/sem-3/codes.constant.ts
+++ src/lib/constants/sem-3/codes.constant.ts
@@ -1,1 +1,46 @@
-export const sem3PracticalCodes = [];
+  export const sem3PracticalCodes = [
+    {
+      subject_name: "Artificial Intelligence",
+      key: "artificial-intelligence",
+      specialization: "ai",
+      language: "python",
+      practicals: [
+        {
+          key: "tic-tac-toe",
+          name: "Practical - 1: Tic Tac Toe",
+          pageBlocks: [
+            {
+              type: "heading",
+              value: "Practical 1 - Tic Tac Toe",
+            },
+            {
+              type: "code",
+              fileName: "tic-tac-toe.py",
+              value: `import random as rd
+
+print("Guessing the number")
+print("Range of Numbers")
+
+start = int(input("enter starting point :"))
+end = int(input("enter ending point :"))
+
+
+
+think = rd.randint(start,end);
+
+while True:
+    guess = int(input("Guess the number :"))
+    if guess > think:
+        print("Guess a lower number :")
+    elif guess < think:
+        print("Guess a higher number :")
+    else:
+        print("You have guessed right number")
+        break;
+`
+            }
+          ]
+        }
+        ]
+        }
+  ];
