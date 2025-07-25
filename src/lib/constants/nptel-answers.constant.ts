cc39a21d018a08f248ee1cee7309339076cef51b
ayushpathak-48 <ayush010pathak@gmail.com>
2025-07-25 10:34:21 +0530

    nptel week 2 assignments answers

--- src/lib/constants/nptel-answers.constant.ts
+++ src/lib/constants/nptel-answers.constant.ts
@@ -57,4 +57,42 @@ export const nptelAllWeeksAnswers = [
       },
     ],
   },
+  {
+    key: "week-2",
+    title: "Week 2 - Assignment answers",
+    answers: [
+      { id: 1, option: 1, answer: "Descriptive and Inferential Statistics" },
+      { id: 2, option: [1, 3, 4], answer: ["Mean", "Median", "Mode"] },
+      { id: 3, option: 2, answer: "Interquartile Range (IQR)" },
+      { id: 4, option: 3, answer: "Cluster Sampling" },
+      {
+        id: 5,
+        option: [1, 2, 3, 4],
+        answer: ["T-test", "Z-test", "Chi-square Test", "ANOVA"],
+      },
+      { id: 6, option: 2, answer: "Statistical significance" },
+      { id: 7, option: 2, answer: "Two-sample t-test" },
+      { id: 8, option: 3, answer: "P(A|B)" },
+      { id: 9, option: 2, answer: "Multiplication Rule" },
+      {
+        id: 10,
+        option: [1, 2, 3, 4],
+        answer: [
+          "Probability of Impossible Event is 0",
+          "Probability of Certain Event is 1",
+          "Addition Rule",
+          "Multiplication Rule",
+        ],
+      },
+      { id: 11, option: 1, answer: "N - 1" },
+      { id: 12, option: 3, answer: "Qualitative Nominal" },
+      { id: 13, option: 2, answer: "Frequency Plot" },
+      { id: 14, option: 2, answer: "Testing for no specific direction" },
+      {
+        id: 15,
+        option: [1, 2, 3, 4],
+        answer: ["Minimum", "Maximum", "Median", "Quartiles"],
+      },
+    ],
+  },
 ];
