34aecd400a988944c3943530148afec47b34392d
ayushpathak-48 <ayush010pathak@gmail.com>
2025-07-27 22:28:11 +0530

    add assignment que 7 for fswd div-i

--- src/lib/constants/sem-3/assignment.constant.ts
+++ src/lib/constants/sem-3/assignment.constant.ts
@@ -6,4 +6,9 @@ export const sem3Assignments = [
     title: "Fullstack Web Development - Div I",
     href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/fsw-assignment.pdf`,
   },
+  {
+    id: 2,
+    title: "Fullstack Web Development - Div I",
+    href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/fsw-assignment-que-7.pdf`,
+  },
 ];
