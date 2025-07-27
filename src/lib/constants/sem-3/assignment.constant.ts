ecbfc6219b151633e00ad173bb6d8d739179bcdc
ayushpathak-48 <ayush010pathak@gmail.com>
2025-07-27 22:30:35 +0530

    change labels of assignments fswd

--- src/lib/constants/sem-3/assignment.constant.ts
+++ src/lib/constants/sem-3/assignment.constant.ts
@@ -3,12 +3,12 @@ import { MATERIAL_LINK_BASE_URL_SEM_3 } from "..";
 export const sem3Assignments = [
   {
     id: 1,
-    title: "Fullstack Web Development - Div I",
+    title: "Fullstack Web Development (Que 1-6) - Div I",
     href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/fsw-assignment.pdf`,
   },
   {
     id: 2,
-    title: "Fullstack Web Development - Div I",
+    title: "Fullstack Web Development (Que-7) - Div I",
     href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/fsw-assignment-que-7.pdf`,
   },
 ];
