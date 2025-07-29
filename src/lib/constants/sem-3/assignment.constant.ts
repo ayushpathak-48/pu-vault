5863817b49256bd69022c518354690aa9cefbd77
ayushpathak-48 <ayush010pathak@gmail.com>
2025-07-29 23:32:15 +0530

    add mad 15 questions assignment pdf

--- src/lib/constants/sem-3/assignment.constant.ts
+++ src/lib/constants/sem-3/assignment.constant.ts
@@ -2,17 +2,22 @@ import { MATERIAL_LINK_BASE_URL_SEM_3 } from "..";
 
 export const sem3Assignments = [
   {
+    id: 4,
+    title: "MAD Assignment - 15 Questions.",
+    href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/mad-assignment-15-questions.pdf`,
+  },
+  {
     id: 3,
     title: "Artificial Intelligence Assignment",
     href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/ai-assignment.pdf`,
   },
   {
-    id: 1,
+    id: 2,
     title: "Fullstack Web Development (Que 1-6) - Div I",
     href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/fsw-assignment.pdf`,
   },
   {
-    id: 2,
+    id: 1,
     title: "Fullstack Web Development (Que-7) - Div I",
     href: `${MATERIAL_LINK_BASE_URL_SEM_3}/assignments/fsw-assignment-que-7.pdf`,
   },
