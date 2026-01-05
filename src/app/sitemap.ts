87e83dd67639e918456d2adc5127988c83ec0c9c
ayushpathak-48 <ayush010pathak@gmail.com>
2026-01-05 23:10:47 +0530

    add pages to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,6 +10,66 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1.0,
     },
     {
+      url: `${baseUrl}/placement-materials`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/software-links`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/assignments`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/nptel-assignment-answers`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/attendance-calculator`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/settings`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/code`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/flash-cards`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/mcqs`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/message-generator`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/professors`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/interview`,
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
       priority: 0.8,
